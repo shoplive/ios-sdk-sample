d9e9bd1b98fb040515d11ce6e06122c46afa5395
whkim <cheripod7@gmail.com>
2021-07-16 22:02:13 +0900

    seperate by min version

--- build.sh
+++ build.sh
@@ -2,21 +2,21 @@
 
 WORKING_DIR=$(pwd)
 
-xcodebuild archive -scheme ShopLiveSDK -archivePath "${WORKING_DIR}/build/combine/ios.xcarchive" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
-xcodebuild archive -scheme ShopLiveSDK -archivePath "${WORKING_DIR}/build/combine/ios_sim.xcarchive" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -scheme ShopLiveSDK -archivePath "${WORKING_DIR}/build/min13/ios.xcarchive" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -scheme ShopLiveSDK -archivePath "${WORKING_DIR}/build/min13/ios_sim.xcarchive" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 # XCFramework 생성
 xcodebuild -create-xcframework \
--framework "${WORKING_DIR}/build/combine/ios.xcarchive/Products/Library/Frameworks/ShopLiveSDK.framework" \
--framework "${WORKING_DIR}/build/combine/ios_sim.xcarchive/Products/Library/Frameworks/ShopLiveSDK.framework" \
--output "${WORKING_DIR}/build/combine/ShopLiveSDK.xcframework"
+-framework "${WORKING_DIR}/build/min13/ios.xcarchive/Products/Library/Frameworks/ShopLiveSDK.framework" \
+-framework "${WORKING_DIR}/build/min13/ios_sim.xcarchive/Products/Library/Frameworks/ShopLiveSDK.framework" \
+-output "${WORKING_DIR}/build/min13/ShopLiveSDK_MinVer13.xcframework"
 
-xcodebuild archive -workspace 'ShopLiveSDK.xcworkspace' -scheme ShopLiveSDKRx -archivePath "${WORKING_DIR}/build/rxswift/ios.xcarchive" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
-xcodebuild archive -workspace 'ShopLiveSDK.xcworkspace' -scheme ShopLiveSDKRx -archivePath "${WORKING_DIR}/build/rxswift/ios_sim.xcarchive" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -workspace 'ShopLiveSDK.xcworkspace' -scheme ShopLiveSDKRx -archivePath "${WORKING_DIR}/build/min11/ios.xcarchive" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -workspace 'ShopLiveSDK.xcworkspace' -scheme ShopLiveSDKRx -archivePath "${WORKING_DIR}/build/min11/ios_sim.xcarchive" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 xcodebuild -create-xcframework \
--framework "${WORKING_DIR}/build/rxswift/ios.xcarchive/Products/Library/Frameworks/ShopLiveSDK.framework" \
--framework "${WORKING_DIR}/build/rxswift/ios_sim.xcarchive/Products/Library/Frameworks/ShopLiveSDK.framework" \
--output "${WORKING_DIR}/build/rxswift/ShopLiveSDK.xcframework"
+-framework "${WORKING_DIR}/build/min11/ios.xcarchive/Products/Library/Frameworks/ShopLiveSDK.framework" \
+-framework "${WORKING_DIR}/build/min11/ios_sim.xcarchive/Products/Library/Frameworks/ShopLiveSDK.framework" \
+-output "${WORKING_DIR}/build/min11/ShopLiveSDK_MinVer11.xcframework"
 
-open "${WORKING_DIR}"
+open "${WORKING_DIR}/build/"
